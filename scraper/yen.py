edece755a8e3961aea065a56b0467db95b6c746c
D2 <dsmontecastro1@up.edu.ph>
2023-10-25 15:12:57 +0800

    [YEN] Finished Scraper

--- scraper/yen.py
+++ scraper/yen.py
@@ -1,4 +1,5 @@
-import time
+import re
+
 from selenium.webdriver.common.by import By as BY
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
@@ -40,6 +41,39 @@ class Book(object):
 # endregion --------------------------------------------------------------------------------------------------
 
 
+# region : Helper Functions ----------------------------------------------------------------------------------
+
+def _attr(elem: WebElement, attr: str = 'innerText') -> str:
+    return elem.get_attribute(attr) or ''
+
+def _getCredits(elems: list[WebElement]) -> list[str]:
+    
+    credits = []
+
+    for elem in elems:
+        name = elem.text
+        if name not in credits: credits.append(name)
+        
+    return credits
+
+def _getTitle(title: str) -> str:
+    title = re.sub(r'\((.*?)\)', '', title)
+    return title.replace('  ', ' ')
+
+def _getPrice(text: str) -> str:
+
+    prices = text.split(' ')
+
+    index = 0
+    try: index = prices.index('US') - 1
+    except ValueError: pass
+
+    return prices[index]
+
+# endregion --------------------------------------------------------------------------------------------------
+
+
+
 def scrape(driver: WebDriver) -> list[Entry]:
 
     print(f'Extracting from {URL}...')
@@ -55,19 +89,24 @@ def scrape(driver: WebDriver) -> list[Entry]:
 
 
     # Process the next <MONTHS> months' Book Pages
+    month: WebElement = Waiter(calendar, WAIT, POLL).until(
+        EC.visibility_of_element_located((CSS, ACTIVE))
+    )
+
     i = 0
-    urls: list[str] = []
-    month = calendar.find_element(CSS, ACTIVE)
+    urls: set[str] = set()
     while i < MONTHS:
+
+        print(month.find_element(CSS, 'h3').text)
 
         # Scroll-To and Click MONTH button
         driver.execute_script('arguments[0].scrollIntoView();', heading)
-        sleep(3)
+        sleep(5)
         month.click()
 
         # Scroll-Down to load all Pages
         driver.execute_script(SCROLL_DOWN)
-        sleep(3)
+        sleep(10)
 
         # Get all Book Page URLs
         for page in driver.find_elements(CSS, 'div.releases-append > div.book-section > div > a'):
@@ -77,36 +116,106 @@ def scrape(driver: WebDriver) -> list[Entry]:
             if format.lower() == 'audio' or format.lower() == 'novels':
 
                 # Check if Book Page URL is valid
-                url = page.get_attribute('href') or ''
-                if url: urls.append(url)
+                url = _attr(page, 'href')
+                if url:
+                    urls.add(url)
+                    i = MONTHS
 
-        # Error Handling
+        # Move to next month
         while True:
 
-            # Move to next month
-            try: month = month.find_element(PTH, 'following-sibling::div[1]')
+            try:
+
+                # Find remaining Months excluding current/active
+                months = calendar.find_elements(CSS, f'{ACTIVE} ~ div')
+
+                if len(months) > 0: month = months[0]   # Get next existing Month
+                else: i = MONTHS                        # Break Outer-Loop
+                break                                   # Break Inner-Loop
 
             # Refresh stale HTML elements
             except StaleElementReferenceException:
                 body = driver.find_element(CSS, 'div.calendar-wrapper')
                 calendar = body.find_element(CSS, 'div.calendar-slider')
                 heading = driver.find_element(CSS, 'div.releases-heading')
-                month = calendar.find_element(CSS, ACTIVE)
-
-            # Break loop on last month
-            except NoSuchElementException: break
-
+    
         i += 1
 
 
     # Process all Book Pages
     entries: list[Entry] = []
-    for url in urls:
+    for url in list(urls)[:3]:
 
         driver.get(url)
 
         if not driver.title.lower().startswith('page not found'):
+
+            sleep(10)
+
             print(f'@{driver.current_url}')
+            page = driver.find_element(CSS, 'div.books-page')
+            info = page.find_element(CSS, 'section.book-cover > div.content-heading > div.book-info')
+            heading = page.find_element(CSS, 'section.series-heading > div.wrapper-1595 > div.heading-content')
+
+            # Heading Elements
+            title = heading.find_element(CSS, 'h1.heading').text
+            names = heading.find_elements(CSS, 'div.story-details > p > span')
+            credits = _getCredits(names)
+
+            # Detail Elements
+            details = page.find_element(CSS, 'section.book-details')
+            detail = details.find_element(CSS, 'div.detail.active')
+            isbns = details.find_elements(PTH, '//span[text()="ISBN"]/following-sibling::p')
+
+            # Detail: Date
+            release = detail.find_element(PTH, '//span[text()="Release Date"]/following-sibling::p')
+            date = _attr(release)
+
+            # Detail: Genres
+            tags = detail.find_elements(CSS, 'div.txt-hold > div.desktop-only > a')
+            genres = [ _attr(tag) for tag in tags]
+
+            # Info: Cover
+            image = info.find_element(CSS, 'div.series-cover > div.book-cover-img > img')
+            cover = _attr(image, 'src')
+
+            # Info: Blurb
+            blurbs = info.find_elements(CSS, 'div.content-heading-txt > *')
+            blurb = '\n'.join([ blurb.text for blurb in blurbs ])
+
+            # Info: Formats
+            buy = info.find_element(CSS, 'div.buy-info')
+            prices = buy.find_elements(CSS, 'div.deliver-info > p')
+            formats = buy.find_elements(CSS, 'div.tabs > span')
+
+            # Mixed: Media
+            media: list[Media] = []
+            for i in range(len(formats)):
+                try:
+                    media.append(
+                        Media(
+                            isbn = _attr(isbns[i]),
+                            format = _attr(formats[i]).lower(),
+                            price = _getPrice(_attr(prices[i])),
+                        )
+                    )
+
+                except (IndexError, NoSuchElementException): continue
+
+
+            entries.append(
+                Entry(
+                    url = url,
+                    date = date,
+                    title = _getTitle(title),
+                    cover = cover,
+                    blurb = blurb,
+                    genres = genres,
+                    credits = credits,
+                    media = media,
+                    table = TABLE
+                )
+            )
 
 
     return entries
