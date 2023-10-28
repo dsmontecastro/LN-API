f79a6c399281b3a5e45293a990df8d38793f3c51
D2 <dsmontecastro1@up.edu.ph>
2023-10-28 16:47:39 +0800

    [TABLES] Prototyped Table-Endpoints

--- pages/blueprints/tables.py
+++ pages/blueprints/tables.py
@@ -1,17 +1,19 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
-# from ...models.table import Tables
+from models.table import Tables
+
 
 tables = Blueprint('table', __name__, url_prefix = '/table', template_folder = 'templates')
 
-@tables.route('/ciw')
-def ciw():
-    try: return render_template('base.html')
-    except TemplateNotFound: abort(404)
+@tables.route('/<code>')
+def table(code: str):
 
+    table = Tables[code.upper()]
+    name = table.name
+    title = table.value
 
-@tables.route('/jnc')
-def jnc():
+    print(name, title)
+
     try: return render_template('base.html')
     except TemplateNotFound: abort(404)
