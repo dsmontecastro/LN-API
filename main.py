3b800ce83774841e359b560eeca173a156724312
D2 <dsmontecastro1@up.edu.ph>
2023-11-30 13:11:09 +0800

    [MAIN] Updated commands

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from app import App
 
-from models.table import Tables
+from app.database.models.table import Tables
 
 
 if __name__ == '__main__':
     app = App(proxy = '', headless = True)
-
-    app.run(Tables.CIW)
-
-    # tables = [ Tables.CIW, Tables.KOD ]
-    # app.test(tables)
-
+    app.run(Tables.ALL, limit = 10)
     app.quit()
