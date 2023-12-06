70a069d1cc7e75de0c134485af563a98aa293ca3
D2 <dsmontecastro1@up.edu.ph>
2023-12-06 17:37:18 +0800

    [ENTRY] Added Entry-relevant files

---
+++ app/pages/blueprints/entry.py
@@ -0,0 +1,46 @@
+from flask import Blueprint, Response, render_template, abort
+from jinja2 import TemplateNotFound
+from json import dumps as to_json
+
+from ._mode import MODE
+from ..singletons import db
+from ...common.logger import log
+
+
+# region : Blueprint -----------------------------------------------------------------------------------------
+
+entry = Blueprint('entry', __name__, url_prefix = '/entry/', template_folder = 'templates')
+
+@entry.route('/<string:mode>/<string:id>')
+def page(mode: str, id: str):
+    
+    try:
+
+        entry = db.get_entry(id)
+
+        if not entry: raise TypeError
+
+
+        match(MODE[mode.upper()]):
+
+            case MODE.JSON:
+                return Response(
+                    to_json(entry, default = str),
+                    mimetype = 'application/json'
+                )
+
+            case MODE.SHOW:
+                return render_template(
+                    'blueprints/entry.html',
+                    entry = entry
+                )
+
+            case _:
+                raise( TemplateNotFound(
+                    name = 'Invalid URL',
+                    message = 'Given MODE does not exist...'
+                ))
+
+    except (TypeError, TemplateNotFound): abort(404)
+
+# endregion --------------------------------------------------------------------------------------------------
