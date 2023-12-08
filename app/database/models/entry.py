57c624b2ca12a4df052cdaae04fde7b8c5412fa2
D2 <dsmontecastro1@up.edu.ph>
2023-12-08 17:41:21 +0800

    [MEDIA] Isolated Media-Class to own module

--- app/database/models/entry.py
+++ app/database/models/entry.py
@@ -2,6 +2,7 @@ from enum import Enum
 from dateutil.parser import parse as dparse
 from datetime import datetime, date as Date
 
+from .media import Media
 from .table import Tables
 
 class Opts(Enum):
@@ -31,29 +32,6 @@ class Fields(Enum):
             cls.FORMAT.value: '{string}',
             cls.PRICE.value: '${X.X}',
             cls.ISBN.value: '{ISBN-Code}'
-        }
-
-
-
-class Media(object):
-
-    def __init__(self, format: str, isbn: str, price: str):
-        self.format = format.lower()
-        self.isbn = isbn.lower()
-        self.price = price
-
-    def __str__(self):
-        return f"""
-            {self.format}
-            ISBN: {self.isbn}
-            PRICE: {self.price}
-        """
-
-    def json(self):
-        return {
-            'format': self.format,
-            'price': self.price,
-            'isbn': self.isbn
         }
 
 
