58cdbfb22bf6a0c4ebd93dac925193ce55765e0f
D2 <dsmontecastro1@up.edu.ph>
2023-12-12 23:51:47 +0800

    [PRICE] Changed to Decimal128-Type

--- app/database/models/media.py
+++ app/database/models/media.py
@@ -1,9 +1,14 @@
+from bson import Decimal128
+
+from ...common.logger import log
+
+
 class Media(object):
 
     def __init__(self, format: str, isbn: str, price: str):
         self.format = format.lower()
         self.isbn = isbn.lower()
-        self.price = price
+        self.set_price(price)
 
     def __str__(self):
         return f"""
@@ -18,3 +23,19 @@ class Media(object):
             'price': self.price,
             'isbn': self.isbn
         }
+
+    def set_price(self, text: str):
+
+        try:
+
+            if text:
+                if text[0] == '$': text = text[1:]
+                if '.' not in text: text += '.00'
+                price = text
+
+            else: price = 'NaN'
+
+            self.price = Decimal128(price)
+        
+        except:
+            self.price = Decimal128('NaN')
--- app/pages/blueprints/_mode.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-
-class MODE(Enum):
-    JSON = 'json'
-    SHOW = 'show'
