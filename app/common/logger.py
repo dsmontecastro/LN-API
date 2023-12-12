5ca8ff42c3b711e925f9f5313f9813b8d9ff7332
D2 <dsmontecastro1@up.edu.ph>
2023-12-12 23:48:50 +0800

    [LOG] Added Logging-File

--- app/common/logger.py
+++ app/common/logger.py
@@ -1,13 +1,22 @@
 import logging
-from selenium.webdriver.remote.remote_connection import LOGGER
 
 # Disable Selenium-based Logging
 logging.getLogger('selenium').setLevel(logging.CRITICAL)
 logging.getLogger('urllib3').setLevel(logging.CRITICAL)
 
 
+FILE = 'logs.txt'
 NAME = 'base_logger'
-FORMAT = '[%(levelname)s] : %(message)s'
+FRMT = '%(asctime)s : [%(levelname)s] : %(message)s'
 
-logging.basicConfig(format = FORMAT, level = logging.DEBUG)
+logging.basicConfig(
+    format = FRMT,
+    # filemode = 'a',
+    # filename = 'logs.txt',
+    level = logging.DEBUG
+)
+
 log = logging.getLogger(NAME)
+
+# console = logging.StreamHandler()
+# log.addHandler(console)
